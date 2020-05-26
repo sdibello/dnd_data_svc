e1ec852c7187834300956c95905db22e4b53163b
Steve DiBello <sdibello@hotmail.com>
2020-05-26 07:55:53 -0400

    updated spellquery with proper logging, and 404 status return

--- dnd_dal/query/spell/SpellQuery.cs
+++ dnd_dal/query/spell/SpellQuery.cs
@@ -27,9 +27,16 @@ namespace dnd_dal.query.spell
         /// <returns>A list of SpellClassLevels </returns>
         public List<SpellClassLevel> ByClassAndLevel(string CasterClass, string CasterLevel)
         {
+            Console.WriteLine(string.Format("log - SpellQuery - ByClassAndLevel PARAMS {0} {1}", CasterClass, CasterLevel));
             var preQuery = _context.DndCharacterclass.Where(cc => cc.Slug == CasterClass.ToLower()).ToList();
 
             var cclass = preQuery.FirstOrDefault();
+
+            if (cclass == null)
+            {
+                Console.WriteLine(string.Format("log - SpellQuery - ByClassAndLevel - Character Class Not Found", CasterClass, CasterLevel));
+                return null;
+            }
 
             var query = _context.DndSpellclasslevel.Where(scl => scl.CharacterClassId == cclass.Id && scl.Level == long.Parse(CasterLevel))
                 .Join(
@@ -59,12 +66,13 @@ namespace dnd_dal.query.spell
                 .OrderBy(g => g.ClassId)
                 .ToList();
 
-            Console.WriteLine(string.Format("log - searchSpellByClassAndLevel - casterClass = {0}/{1}", CasterClass, CasterLevel));
             if (query != null)
             {
+                Console.WriteLine(string.Format("log - SpellQuery - ByClassAndLevel - ByClassAndLevel results {0}", query.Count()));
                 return query;
             };
 
+            Console.WriteLine(string.Format("log - SpellQuery - ByClassAndLevel - Not Spells Found", CasterClass, CasterLevel));
             return null;
         }
 
--- dnd_graphql_svc/Controllers/SpellsController.cs
+++ dnd_graphql_svc/Controllers/SpellsController.cs
@@ -162,45 +162,19 @@ namespace dnd_graphql_svc.Controllers
             var query = new SpellQuery(_context);
             var results = query.ByClassAndLevel(casterClass, casterlevel);
 
-            //var preQuery = _context.DndCharacterclass.Where(cc => cc.Slug == casterClass.ToLower()).ToList();
-
-            //var cclass = preQuery.FirstOrDefault();
-
-            //var query = _context.DndSpellclasslevel.Where(scl => scl.CharacterClassId == cclass.Id && scl.Level == long.Parse(casterlevel))
-            //    .Join(
-            //        _context.DndSpell,
-            //        cl => cl.SpellId,
-            //        s => s.Id,
-            //        (cl, s) => new SpellClassLevel
-            //        {
-            //            SpellId = s.Id,
-            //            ClassId = cl.CharacterClassId,
-            //            Level = cl.Level,
-            //            SpellName = s.Name
-            //        }
-            //    )
-            //    .Join(
-            //        _context.DndCharacterclass,
-            //        cl => cl.ClassId,
-            //        cc => cc.Id,
-            //        (cl, cc) => new SpellClassLevel
-            //        {
-            //            SpellId = cl.SpellId,
-            //            ClassId = cc.Id,
-            //            Level = cl.Level,
-            //            ClassName = cc.Name,
-            //            SpellName = cl.SpellName
-            //        })
-            //    .OrderBy(g => g.ClassId)
-            //    .ToList();
-
             if (results != null)
             {
-                Console.WriteLine(string.Format("log - searchSpellByClassAndLevel - casterClass = {0}/{1} - returned {2} results", casterClass, casterlevel, results.Count()));
-                return results;
+                if (results.Count != 0)
+                {
+                    Console.WriteLine(string.Format("log - searchSpellByClassAndLevel - SpellController = {0}/{1} - returned {2} results", casterClass, casterlevel, results.Count()));
+                    return results;
+                } else {
+                    Console.WriteLine(string.Format("log - searchSpellByClassAndLevel - SpellController - NO RESULTS", casterClass, casterlevel));
+                    return NotFound();
+                }
             };
 
-            Console.WriteLine(string.Format("log - searchSpellByClassAndLevel - casterClass = {0}/{1} - NOT FOUND", casterClass, casterlevel));
+            Console.WriteLine(string.Format("log - searchSpellByClassAndLevel - SpellController = {0}/{1} - NOT FOUND", casterClass, casterlevel));
             return NotFound();
         }
 
